8ffe12adf440aaa5275615e6ad8292a741aab88c
yoobni <yoobni1225@gmail.com>
2022-05-28 00:11:30 +0900

    Feat: add grid

--- src/containers/Summoners/index.tsx
+++ src/containers/Summoners/index.tsx
@@ -1,7 +1,7 @@
 // Package
 import React from 'react';
 // Component
-import { Wrapper, GridWrapper, Text } from "../../components/Layout";
+import { Wrapper, GridWrapper, Row, Col, Text } from "../../components/Layout";
 import {
     ContentHeader,
 } from '../../components/Summoners';
@@ -9,12 +9,32 @@ import {
 function Summoner() {
     return (
         <>
-            <ContentHeader>
-
-            </ContentHeader>
+            <ContentHeader />
             <Wrapper height={'calc(100vh - 200px)'}>
                 <GridWrapper>
+                    <Row className={'main'} padding={'10px 0 0'}>
+                        <Col className={'tier-championRate'}>
+                            <Row className={'tierBox'}>
 
+                            </Row>
+                            <Row className={'championRate'}>
+
+                            </Row>
+                        </Col>
+                        <Col className={'rankGame-gameList'}>
+                            <Row className={'rankGameMenu'}>
+                                <Row className={'rank-menubar'}>
+
+                                </Row>
+                                <Row className={'rank-chart'}>
+
+                                </Row>
+                            </Row>
+                            <Row className={'rankGameList'}>
+
+                            </Row>
+                        </Col>
+                    </Row>
                 </GridWrapper>
             </Wrapper>
         </>
