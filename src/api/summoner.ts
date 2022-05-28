d4d60139975eb406c54251465ca9076bc2cf3e4c
yoobni <yoobni1225@gmail.com>
2022-05-28 18:16:30 +0900

    Feat: add api call

--- src/api/summoner.ts
+++ src/api/summoner.ts
@@ -1,12 +1,17 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import { summonerApi } from "../lib/request";
 
-export interface GetSummonerParameter {
+export interface GetSummonerNameParameter {
     summonerName: string;
 }
 
+export interface GetSummonerGameParameter {
+    summonerName: string;
+    gameId: number | string;
+}
+
 // TODO: axios success callback function
-const errorException = (err) => {
+const errorException = (err: any) => {
     const errors = err as Error | AxiosError;
 
     if (axios.isAxiosError(errors)) {
@@ -19,10 +24,51 @@ const errorException = (err) => {
 };
 
 export async function getSummoner(
-    _req: AxiosRequestConfig & GetSummonerParameter,
+    _req: AxiosRequestConfig & GetSummonerNameParameter,
 ): Promise<null> {
     try {
         const response: AxiosResponse<any> = await summonerApi.get(`/api/summoner/${_req.summonerName}`);
+
+        return response.data;
+    } catch (err) {
+        return errorException(err);
+    }
+}
+
+export async function getMatchDetail(
+    _req: AxiosRequestConfig & GetSummonerGameParameter,
+): Promise<null> {
+    const {
+        summonerName,
+        gameId,
+    } = _req;
+
+    try {
+        const response: AxiosResponse<any> = await summonerApi.get(`/api/summoner/${summonerName}/matchDetail/${gameId}`);
+
+        return response.data;
+    } catch (err) {
+        return errorException(err);
+    }
+}
+
+export async function getMatches(
+    _req: AxiosRequestConfig & GetSummonerNameParameter,
+): Promise<null> {
+    try {
+        const response: AxiosResponse<any> = await summonerApi.get(`/api/summoner/${_req.summonerName}/matches`);
+
+        return response.data;
+    } catch (err) {
+        return errorException(err);
+    }
+}
+
+export async function getMostInfo(
+    _req: AxiosRequestConfig & GetSummonerNameParameter,
+): Promise<null> {
+    try {
+        const response: AxiosResponse<any> = await summonerApi.get(`/api/summoner/${_req.summonerName}/mostInfo`);
 
         return response.data;
     } catch (err) {
--- src/pages/summoners/[summoner].tsx
+++ src/pages/summoners/[summoner].tsx
@@ -1,24 +1,24 @@
 // Package
-import React, {Component} from 'react';
+import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Summoner from '../../containers/Summoners';
 import { Header, Footer } from "../../components/Template";
 
-class SummonerPage extends Component {
-    render() {
-        return (
-            <>
-                <Header />
-                <Head>
-                    <meta property="apptitle" content={`Summoner님의 - 게임 전적 - League of Legends`} />
-                    <title>Summoner님의 - 게임 전적 - League of Legends</title>
-                </Head>
-                <Summoner />
-                <Footer />
-            </>
-        )
-    }
+function SummonerPage() {
+    const summonerName = "test";
+
+    return (
+        <>
+            <Header />
+            <Head>
+                <meta property="apptitle" content={`${summonerName}님의 - 게임 전적 - League of Legends`} />
+                <title>{summonerName}님의 - 게임 전적 - League of Legends</title>
+            </Head>
+            <Summoner />
+            <Footer />
+        </>
+    )
 }
 
 export default SummonerPage;
